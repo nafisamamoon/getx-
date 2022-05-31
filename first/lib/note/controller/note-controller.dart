8808f7e9ee7638a133207679a234781d2f9a046d
unknown <nafisamamoon11@gmail.com>
2022-05-31 05:27:17 -0700

    2

--- first/lib/note/controller/note-controller.dart
+++ first/lib/note/controller/note-controller.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:first/note/model/note-model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -13,7 +15,9 @@ class NoteController extends GetxController{
   List noteResult=[].obs;
   RxList notes=[].obs;
   RxList doneNotes=[].obs;
-  void done(var ischecked){
+  RxList dis=[].obs;
+  //for checkbox
+ /* void done(var ischecked){
     //doneNotes.clear();
 if(ischecked.status == true){
   doneNotes.add(ischecked);
@@ -26,7 +30,80 @@ if(ischecked.status == true){
    doneNotes.remove(ischecked);
 }
 print(doneNotes.length);
-  }
+  }*/
+  var choice='done'.obs;
+void dismiss(var d){
+  notes.value.remove(d);
+dis.add(d);
+
+}
+void doneList(val){
+if(val == 'done'){
+  Get.defaultDialog(
+    title: 'Accomplished tasks',
+  content:Container(
+    height: 200,
+    child: ListView.builder(
+    itemCount: dis.length,
+    itemBuilder: (context,i){
+      return Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Container(
+              height: 30,
+              alignment: Alignment.center,
+              width: double.infinity,
+              //height: 200,
+             decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(9),
+               color: Colors.orangeAccent
+             ),
+        child: Text('${dis[i].title}')
+        
+            ),
+          ),
+        ],
+      );
+    }
+    ),
+  )
+  );
+}else if(val =='undone'){
+  Get.defaultDialog(
+   
+    title: ' not Accomplished tasks',
+  content:Container(
+    
+    height: 200,
+    child: ListView.builder(
+    itemCount: notes.length,
+    itemBuilder: (context,i){
+      return Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Container(
+              height: 30,
+              alignment: Alignment.center,
+              width: double.infinity,
+              //height: 200,
+             decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(9),
+               color: Colors.orangeAccent
+             ),
+        child: Text('${notes[i].title}')
+        
+            ),
+          ),
+        ],
+      );
+    }
+    ),
+  )
+  );
+}
+}
   void fetchnotes()async{
  noteResult=[
   Note(
--- first/lib/note/view/my-home.dart
+++ first/lib/note/view/my-home.dart
@@ -8,6 +8,63 @@ NoteController controller=Get.put(NoteController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+        title: Text('To-do list'),
+        actions: [
+         
+              DropdownButton(
+                items:['done','undone'].map((e){
+                return  DropdownMenuItem<String>(child: Text("$e"),value: e,);
+                
+                }).toList(),
+               // value:controller.choice.value ,
+               hint: Text(' All Todos'),
+                onChanged: (val){
+controller.doneList(val);
+                },
+              )
+            
+          
+            
+        ],
+      ),
+      body: GetBuilder<NoteController>(
+      builder: (controller){
+return Container(
+  child: ListView.builder(
+    itemCount: controller.notes.length,
+    itemBuilder: (context,i){
+      //var n=controller.notes[i];
+      return Dismissible(
+        onDismissed:(direction){
+           controller.update();
+           controller.dismiss(controller.notes[i]);
+          controller.update();
+         
+        } ,
+        key: ObjectKey(controller.notes[i]), 
+        child: Card(
+          elevation: 9,
+          child: ListTile(
+            title: Text('${controller.notes[i].title}'),
+            subtitle:  Text('${controller.notes[i].content}'),
+          ),
+        )
+        );
+    }
+    ),
+);
+      }
+      ),
+    );
+  }
+}
+//with checkbox
+/*
+return Scaffold(
       
       appBar: AppBar(
         elevation: 0,
@@ -50,5 +107,4 @@ Get.to(()=>Show());
         backgroundColor: Colors.amber,
       )
     );
-  }
-}
+*/
